3ce9ce790c11464b9d85cfe7eeedd1645e395af4
Xapapy <caffiend1999@gmail.com>
2016-10-04 13:57:54 -0400

    10/4

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -16,15 +16,16 @@
 // boolean isObjectmpty()
 /**
  * @version 9/28/16
- * @author bushneca18 
- * @param <E> - The object type the list will hold.
+ * @author bushneca18
+ * @param <E>
+ *            - The object type the list will hold.
  */
 public class LinkedList<E>
 {
-    
+
     private Node<E> head;
     private int size;
-    
+
     /**
      * Default constructor. Creates an empty LinkedList with a head of "null".
      */
@@ -32,150 +33,165 @@ public class LinkedList<E>
     {
         super();
         head = null;
+        size = 0;
     }
-    
+
+    private void locationCheck(int loc)
+    {
+        if (loc > size || loc < 0 || (size == 0 && loc == 0))
+        {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
     /**
      * Adds a node to the end of the list;
-     * @param data - 
+     * 
+     * @param data
+     *            -
      * @return
      */
-    public boolean add( E data )
+    public boolean add(E data)
     {
         if (head == null)
         {
-            head = new Node( data, null );
+            head = new Node(data, null);
+            ++size;
             return true;
-        }
+        } 
         else
         {
             Node temp = head;
-            while ( temp.next != null ) 
+            while (temp.next != null)
             {
                 temp = temp.next;
             }
-            temp.next = new Node( data, null );
+            temp.next = new Node(data, null);
             ++size;
             return true;
         }
     }
-    
+
     /**
      * Returns the size of the LinkedList.
+     * 
      * @return the size of the LinkedList.
      */
     public int size()
     {
-        return size();
+        return size;
     }
-    
+
     /**
      * Adds an object to the LinkedList at a defined location.
-     * @param loc - The location you want to add the object to.
-     * @param o - The object you want to add.
+     * 
+     * @param loc
+     *            - The location you want to add the object to.
+     * @param o
+     *            - The object you want to add.
      */
-    public void add( int loc, E o )
+    public void add(int loc, E o)
     {
         Node temp = head;
-        if ( loc > size || loc < 0 )
+        if (head == null && loc == 0)
         {
-            throw new IndexOutOfBoundsException();
+            
         }
-        else
+        this.locationCheck(loc);
+        int i = 0;
+        while (i < loc)
         {
-            int i = 0;
-            while ( i < loc )
-            {
-                temp = temp.next;
-                ++i;
-            }
-            temp.next = new Node( o, temp.next );
-            ++size;
+            temp = temp.next;
+            ++i;
         }
+        temp.next = new Node(o, temp.next);
+        ++size;
     }
-    
+
     /**
      * Returns the data of the LinkedList in a String form.
      */
     public String toString()
     {
-        if ( size <= 0 )
+        if (size <= 0)
         {
             return "[]";
         }
-        else {
-            StringBuilder finalString = new StringBuilder("[");
-            Node temp = head;
-            while ( temp.next.next != null )
-            {
-                finalString.append( temp.data.toString() + ", " );
-                temp = temp.next;
-            }
-            finalString.append( temp.next.data.toString() + "]" );
-            return finalString.toString();
-            
-        }
-    }
-    
-    public E get( int loc )
-    {
-        Node temp = head;
-        if ( loc > size || loc < 0 )
-        {
-            throw new IndexOutOfBoundsException();
-        }
         else
         {
-            int i = 0;
-            while ( i <= loc )
+            StringBuilder finalString = new StringBuilder("[");
+            Node temp = head;
+            while (temp.next.next != null)
             {
+                finalString.append(temp.data.toString() + ", ");
                 temp = temp.next;
-                ++i;
             }
+            finalString.append(temp.next.data.toString() + "]");
+            return finalString.toString();
+
         }
-        return ( E ) temp.data;
     }
-    
+
+    public E get(int loc)
+    {
+        this.locationCheck(loc);
+        Node<E> temp = head;
+        int i = 0;
+        while (i < loc)
+        {
+            temp = temp.next;
+            ++i;
+        }
+        return (E) temp.data;
+    }
+
     /**
      * @author bushneca18
-     * @param <E> - The data type the ListNode will hold.
+     * @param <E>
+     *            - The data type the ListNode will hold.
      */
     private class Node<E>
     {
         private E data;
         private Node<E> next;
-        
+
         /**
          * Default constructor. Builds an empty node with a next of "null".
          */
         public Node()
         {
             super();
-            new Node( null, null );
+            new Node(null, null);
         }
-        
+
         /**
          * Builds a new Node.
-         * @param data - The data the node will hold. Must be type E.
-         * @param next - The next node in the list.
+         * 
+         * @param data
+         *            - The data the node will hold. Must be type E.
+         * @param next
+         *            - The next node in the list.
          */
-        public Node( E data, Node next )
+        public Node(E data, Node next)
         {
             super();
             this.data = data;
             this.next = next;
         }
-        
+
         /**
          * Returns the next in the list in relation to the current node.
+         * 
          * @return The next node in the list.
          */
         public Node getNext()
         {
             return this.next;
         }
-        
+
         /**
          * Returns the data in the node.
+         * 
          * @return the node's data.
          */
         public E getData()
